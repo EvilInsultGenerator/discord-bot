b0b37cc036d1db2d4abb75ed3f64678005b72fbe
Astrydax <astrydax@gmail.com>
2022-01-20 06:10:00 -0500

    modernized help message

--- commands/help.js
+++ commands/help.js
@@ -5,6 +5,7 @@ module.exports = {
         .setName('help')
         .setDescription('sends help message'),
     async execute(interaction) {
-        await interaction.reply('To generate an insult use /insult \nTo set the language use /language\nTo visit the homepage use /homepage\nTo get help use /help');
+        await interaction.reply("use / to see a list of commands");
+        //await interaction.reply('To generate an insult use /insult \nTo set the language use /language\nTo visit the homepage use /homepage\nTo get help use /help');
     },
 };
