8fee4326efd42cdbac7646cbfebc21c6bafbd0b2
Astrydax <astrydax@gmail.com>
2022-01-23 04:32:20 -0500

    basic functionality

--- commands/language.js
+++ commands/language.js
@@ -1,10 +1,40 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const {MessageActionRow, MessageEmbed, MessageSelectMenu, SelectMenuInteraction} = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('language')
         .setDescription('sets your preferred language'),
     async execute(interaction) {
-        await interaction.reply('To generate an insult: !generate or !insult or !fuck\nTo set the language: !language\nTo visit the homepage: !homepage\nTo get help: !help');
+        const row = new MessageActionRow()
+            .addComponents(
+                new MessageSelectMenu()
+                    .setCustomId('language')
+                    .setPlaceholder('Nothing selected')
+                    .addOptions([
+                        {
+                            label: 'English',
+                            value: 'en',
+                        },
+                        {
+                            label: 'Español',
+                            value: 'es',
+                        }, {
+                            label: 'Deutsch',
+                            value: 'de',
+                        }
+                    ]),
+            );
+
+        await interaction.reply({content: 'Please Select a language.', ephemeral: true, components: [row]});
+    },
+    async executeInteraction(/** MessageComponentInteraction */ interaction, userPrefs){
+        if (!interaction.isSelectMenu()) return;
+        if(interaction.customId === "language"){
+            console.log(interaction.values)
+            await userPrefs.set(interaction.user.id, interaction.values[0]);
+            interaction.reply({content: "Success!", ephemeral: true})
+        }
+
     },
 };
