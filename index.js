8b657dc254a122084f03a8075868ef3c062dc80a
Astrydax <astrydax@gmail.com>
2022-01-23 04:32:06 -0500

    add new db structure to command handler

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
+const Keyv = require('keyv');
+
+const userPrefs = new Keyv('sqlite://users.db');
+userPrefs.on('error', err => console.error('Keyv connection error:', err));
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES] });
+
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -17,20 +22,20 @@ client.once('ready', () => {
 });
 
 client.on('interactionCreate', async interaction => {
-    // if (!interaction.isCommand()) {
-    //
-    //     client.commands.forEach((cmd => {
-    //         cmd.executeInteraction?.(interaction);
-    //     }))
-    //     return;
-    // }
+    if (!interaction.isCommand()) {
+
+        client.commands.forEach((cmd => {
+            cmd.executeInteraction?.(interaction, userPrefs);
+        }))
+        return;
+    }
 
     const command = client.commands.get(interaction.commandName);
 
     if (!command) return;
 
     try {
-        await command.execute(interaction);
+        await command.execute(interaction, userPrefs);
     } catch (error) {
         console.error(error);
         return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
